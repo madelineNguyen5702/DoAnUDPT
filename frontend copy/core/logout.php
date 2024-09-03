a730b13adcc2dd2429d02a4a305622c7bbaeff3f
madelineNguyen5702 <64205732+madelineNguyen5702@users.noreply.github.com>
2024-09-04 01:04:43 +0700

    frontend fix

--- frontend copy/core/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Bắt đầu phiên làm việc
-session_unset(); // Xóa tất cả các biến phiên
-session_destroy(); // Hủy phiên làm việc
-
-// Chuyển hướng đến trang đăng nhập hoặc trang khác
-header("Location: login.html");
-exit();
-?>
